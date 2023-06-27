80c2f6c9751def29ae2b984fe635003280bb84dd
Boris Dovčík <bdovcik@gmail.com>
2023-06-27 20:58:53 +0200

    Include GoCarlos.NET.Tests in Git

---
+++ GoCarlos.NET.Tests/Services/TournamentTest.cs
@@ -0,0 +1,26 @@
+﻿using GoCarlos.NET.Interfaces;
+using GoCarlos.NET.Models;
+
+namespace GoCarlos.NET.Tests.Services;
+
+[TestClass]
+public class TournamentTest
+{
+
+    private ITournament tournament;
+
+    public TournamentTest()
+    {
+        tournament = new Tournament();
+    }
+
+    [TestMethod]
+    public void Test()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
