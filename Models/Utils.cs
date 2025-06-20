c095beda0a582e6888f647eaefd350e757d25f00
Boris Dovčík <bdovcik@gmail.com>
2025-06-20 09:08:10 +0200

    Bump version

--- Models/Utils.cs
+++ Models/Utils.cs
@@ -9,7 +9,7 @@ namespace GoCarlos.NET.Models;
 
 internal static class Utils
 {
-    public const string VERSION = "0.0.9";
+    public const string VERSION = "0.0.10";
     public const string BYE = "0+";
     public const string QUESTION_MARK = "?";
     public const string EQUALS = "=";
